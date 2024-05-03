bb8fe2c916d1a308ff65ac3d8f5bc839cf76de88
Gele Hasan <gellehussen@gmail.com>
2024-05-03 14:48:38 +0200

    added filter section

--- src/Routers/Properties/Properties.jsx
+++ src/Routers/Properties/Properties.jsx
@@ -1,11 +1,29 @@
-import "./index.css"
+import "./index.css";
+import { useState } from "react";
 const Properties = ()=>{
-
+    const [isOpen, setIsOpen] = useState(false); 
+    const toggleDropdown = () => setIsOpen(!isOpen);
     return(
-        <div className="propertiesContainer">
+        <div className="propertiesContainer">´
+        <div className="propertiesContent">
             <div className="searchFilter">
-              
-                <input type="text" placeholder="Search"/>
+            <h3>Search for properties</h3>
+            <div className="filterOperation">
+                        <div class="propertDropdown">
+                            <button className="dropbtn" onClick={toggleDropdown}>
+                                <span>Tidigast tillträde först </span> <span> {isOpen ? '▲' : '▼'}</span>
+                            </button>
+                            <div className={`dropdown-content ${isOpen ? 'show' : ''}`}>
+                                <a href="#">Tidigast tillträde först</a>
+                                <a href="#">Senast publicerad först</a>
+                                <a href="#">Lägst hyra först</a>
+                                <a href="#">Störst först (m2)</a>
+                                <a href="#">Minst först (m2)</a>
+                            </div>
+                        </div>
+            </div>
+            </div>
+
             </div>
         </div>
     )
