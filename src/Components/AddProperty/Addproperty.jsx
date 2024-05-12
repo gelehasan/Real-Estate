8a265b3e205a1b94e0b385f734e22c8b23eb3c6f
Nex Shadow <gellehussen@gmail.com>
2024-05-12 23:38:46 +0200

    formatted

--- src/Components/AddProperty/Addproperty.jsx
+++ src/Components/AddProperty/Addproperty.jsx
@@ -4,173 +4,174 @@ import { AddNewService } from "../../Firebase/firebase";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 const AddProperty = () => {
-  const currentUser = useSelector((state)=> state.user.currentUser)
+    const currentUser = useSelector((state) => state.user.currentUser)
 
-  const [formData, setFormData] = useState({
-    name: "",
-    price:"",
-    location:"",
-    description: "",
-    type:"",
-    images: [],
-    city:"",
-    
-  });
-  const Navigate = useNavigate();
-  const handleInputChange = (name, value) => {
-    
-    setFormData({
-      ...formData,
-      [name]: value,
+    const [formData, setFormData] = useState({
+        name: "",
+        price: "",
+        location: "",
+        description: "",
+        type: "",
+        images: [],
+        city: "",
+
     });
-  };
+    const Navigate = useNavigate();
+    const handleInputChange = (name, value) => {
 
-  const handleAddLista = () => {
-    setFormData({
-      ...formData,
-      images: [...formData.images, { id: formData.images.length, value: "" }],
-    });
-  };
-;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
 
-  const handleListaChange = (id, value) => {
-    const updatedListor = formData.images.map((item) =>
-      item.id === id ? { ...item, value } : item
-    );
-    setFormData({
-      ...formData,
-      images: updatedListor,
-    });
-  };
+    const handleAddLista = () => {
+        setFormData({
+            ...formData,
+            images: [...formData.images, { id: formData.images.length, value: "" }],
+        });
+    };
+    ;
 
-  const handleOtherListChange = (id, value) => {
-    const updatedOtherList = formData.otherList.map((item) =>
-      item.id === id ? { ...item, value } : item
-    );
-    setFormData({
-      ...formData,
-      otherList: updatedOtherList,
-    });
-  };
-  
+    const handleListaChange = (id, value) => {
+        const updatedListor = formData.images.map((item) =>
+            item.id === id ? { ...item, value } : item
+        );
+        setFormData({
+            ...formData,
+            images: updatedListor,
+        });
+    };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    
-    if(currentUser && currentUser.Admin=='true'){
-    
-      try {
-      { await  AddNewService(formData)
-        Navigate("/")
-        window.location.reload();
-    }
-      console.log("Submitted with ", formData)
-      } catch (error) {
-        
-      }
-    }
-  };
+    const handleOtherListChange = (id, value) => {
+        const updatedOtherList = formData.otherList.map((item) =>
+            item.id === id ? { ...item, value } : item
+        );
+        setFormData({
+            ...formData,
+            otherList: updatedOtherList,
+        });
+    };
 
 
-  return (
-    <div className="form-container">
-      <form className="service-form" onSubmit={handleSubmit}>
-        <label> Title</label> <br />
-        <input
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={(e) => handleInputChange(e.target.name, e.target.value)}
-        />
-        <br />
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-        <label> Price</label> <br />
-        <input
-          id="price"
-          type="number"
-          name="price"
-          value={formData.price}
-          onChange={(e) => handleInputChange(e.target.name, e.target.value)}
-        />
-        <br />
-        <label> Location</label> <br />
-        <input
-          id="location"
-          name="location"
-          value={formData.location}
-          onChange={(e) => handleInputChange(e.target.name, e.target.value)}
-        />
-        <br />
-        <label> City</label> <br />
-        <input
-          id="city"
-          name="city"
-          value={formData.city}
-          onChange={(e) => handleInputChange(e.target.name, e.target.value)}
-        />
-        <br />
+        if (currentUser && currentUser.Admin == 'true') {
 
-        <label> Description</label> <br />
-        <input
-          id="description"
-          name="description"
-          value={formData.description}
-          onChange={(e) => handleInputChange(e.target.name, e.target.value)}
-        />
-        <br />
+            try {
+                {
+                    await AddNewService(formData)
+                    Navigate("/")
+                    window.location.reload();
+                }
+                console.log("Submitted with ", formData)
+            } catch (error) {
 
-        <label>Type</label>
-        <div>
-          <input
-            type="radio"
-            id="rent"
-            name="type"
-            value="rent"
-            checked={formData.type === "rent"}
-            onChange={(e)=>handleInputChange(e.target.name,e.target.value)}
-          />
-          <label htmlFor="rent">Rent</label>
+            }
+        }
+    };
 
-          <input
-            type="radio"
-            id="land"
-            name="type"
-            value="land"
-            checked={formData.type === "land"}
-            onChange={(e)=>handleInputChange(e.target.name,e.target.value)}
-          />
-          <label htmlFor="land">Land</label>
 
-          <input
-            type="radio"
-            id="houses"
-            name="type"
-            value="houses"
-            checked={formData.type === "houses"}
-            onChange={(e)=>handleInputChange(e.target.name,e.target.value)}
-          />
-          <label htmlFor="houses">Houses</label>
+    return (
+        <div className="form-container">
+            <form className="service-form" onSubmit={handleSubmit}>
+                <label> Title</label> <br />
+                <input
+                    id="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={(e) => handleInputChange(e.target.name, e.target.value)}
+                />
+                <br />
+
+                <label> Price</label> <br />
+                <input
+                    id="price"
+                    type="number"
+                    name="price"
+                    value={formData.price}
+                    onChange={(e) => handleInputChange(e.target.name, e.target.value)}
+                />
+                <br />
+                <label> Location</label> <br />
+                <input
+                    id="location"
+                    name="location"
+                    value={formData.location}
+                    onChange={(e) => handleInputChange(e.target.name, e.target.value)}
+                />
+                <br />
+                <label> City</label> <br />
+                <input
+                    id="city"
+                    name="city"
+                    value={formData.city}
+                    onChange={(e) => handleInputChange(e.target.name, e.target.value)}
+                />
+                <br />
+
+                <label> Description</label> <br />
+                <input
+                    id="description"
+                    name="description"
+                    value={formData.description}
+                    onChange={(e) => handleInputChange(e.target.name, e.target.value)}
+                />
+                <br />
+
+                <label>Type</label>
+                <div>
+                    <input
+                        type="radio"
+                        id="rent"
+                        name="type"
+                        value="rent"
+                        checked={formData.type === "rent"}
+                        onChange={(e) => handleInputChange(e.target.name, e.target.value)}
+                    />
+                    <label htmlFor="rent">Rent</label>
+
+                    <input
+                        type="radio"
+                        id="land"
+                        name="type"
+                        value="land"
+                        checked={formData.type === "land"}
+                        onChange={(e) => handleInputChange(e.target.name, e.target.value)}
+                    />
+                    <label htmlFor="land">Land</label>
+
+                    <input
+                        type="radio"
+                        id="houses"
+                        name="type"
+                        value="houses"
+                        checked={formData.type === "houses"}
+                        onChange={(e) => handleInputChange(e.target.name, e.target.value)}
+                    />
+                    <label htmlFor="houses">Houses</label>
+                </div>
+                <label>Images</label> <br />
+                {formData.images.map((item) => (
+                    <div key={item.id}>
+                        <textarea
+                            value={item.value}
+                            onChange={(e) => handleListaChange(item.id, e.target.value)}
+                        />
+                    </div>
+                ))}
+                <button type="button" onClick={handleAddLista} className="btnLists">
+                    Add List Item
+                </button>
+                <br />
+
+                <br />
+
+                <button className="addService" type="submit">Submit</button>
+            </form>
         </div>
-        <label>Images</label> <br />
-        {formData.images.map((item) => (
-          <div key={item.id}>
-            <textarea
-              value={item.value}
-              onChange={(e) => handleListaChange(item.id, e.target.value)}
-            />
-          </div>
-        ))}
-        <button type="button" onClick={handleAddLista} className="btnLists">
-          Add List Item
-        </button>
-        <br />
-
-<br />
-
-        <button className="addService" type="submit">Submit</button>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default AddProperty;
