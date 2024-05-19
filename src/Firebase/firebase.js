5bd0bb66ce6afb58ec9e671fa227eab759e249dc
Gele hasan <gellehussen@gmail.com>
2024-05-19 23:51:02 +0200

    replaced config with imported module

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -5,15 +5,8 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 import { onAuthStateChanged } from "firebase/auth";
 import { signOut } from "firebase/auth";
 import { setDoc } from "firebase/firestore";
+import firebaseConfig from "./firebaseconfig";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD0SG_OkjOq9JejF8FCgNmMUm8h-tTz5yM",
-  authDomain: "gadooy-1f7be.firebaseapp.com",
-  projectId: "gadooy-1f7be",
-  storageBucket: "gadooy-1f7be.appspot.com",
-  messagingSenderId: "517192318165",
-  appId: "1:517192318165:web:978ab4266812f9a75b765c"
-};
 
 const app = initializeApp(firebaseConfig);
 
