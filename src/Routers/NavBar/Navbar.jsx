5bf32d3a4a3cfc23997bae014f06128c7a4e06fb
Gele hasan <gellehussen@gmail.com>
2024-05-21 23:43:08 +0200

    format improvement

--- src/Routers/NavBar/Navbar.jsx
+++ src/Routers/NavBar/Navbar.jsx
@@ -7,33 +7,33 @@ import { useLocation } from 'react-router-dom';
 import { useSelector } from "react-redux";
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
-    const location=useLocation();
-    const currentUser = useSelector((state)=>state.user.currentUser);
+    const location = useLocation();
+    const currentUser = useSelector((state) => state.user.currentUser);
     const homePage = location.pathname === "/";
 
 
     const handleToggle = () => {
-      setIsOpen(!isOpen);
+        setIsOpen(!isOpen);
     };
 
     return (
-        <nav className={homePage ? "navbars navHomePage" :  "navbars"}>
+        <nav className={homePage ? "navbars navHomePage" : "navbars"}>
             <div className="nav-container">
                 {/* Logo on the left side */}
                 <div className="nav-logo">
-                   <img src={logo}/>
+                    <img src={logo} />
                 </div>
 
                 {/* Toggle button for small screens */}
-                <img className={isOpen ? "nav-toggles" : "nav-toggle"} onClick={handleToggle} src={ isOpen ? CloseMenu : menu}/>
-            
+                <img className={isOpen ? "nav-toggles" : "nav-toggle"} onClick={handleToggle} src={isOpen ? CloseMenu : menu} />
+
 
                 {/* Navigation Links */}
                 <div className={`nav-items ${isOpen ? 'open' : ''}`}>
                     <a href="/">Home</a>
                     <a href="/properties">Properties</a>
-                    {currentUser && <a href="/admin-dashboard"> Admin</a> }
-                   
+                    {currentUser && <a href="/admin-dashboard"> Admin</a>}
+
                     <a href="/listed">About</a>
                     <a href="/contact">Contact</a>
                 </div>
